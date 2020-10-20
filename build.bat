0f202aa6fd7b90253c4dc82b58903a2f151ec669
Jarl Ostensen <jarl@polystream.com>
2020-10-20 21:27:50 +0100

    minor cleanups and re-names

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ cd build
 rem ZZZ: until I figure out why build.zig doesn't work with assembly source I need to do this manually
 ..\zig\zig clang -c ../kernel/arch/x86_64/kernel.s
 if %ERRORLEVEL% equ 0 (
-    ..\zig\zig.exe build-exe ../kernel/efi_main.zig --object kernel.o --name bootx64 -target x86_64-uefi-msvc --subsystem efi_application -femit-asm
+    zig build-exe ../kernel/efi_main.zig --object kernel.o --name bootx64 -target x86_64-uefi-msvc --subsystem efi_application -femit-asm --verbose-llvm-cpu-features
 
     if %ERRORLEVEL% equ 0 (
         ..\tools\efigen.exe -i bootx64.efi -o boot.dd 
